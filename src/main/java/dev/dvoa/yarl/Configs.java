b9fd7a05ecd0cde978a30000392930c20a578dfa
DVOA1 <88774071+DVOA1@users.noreply.github.com>
2026-02-20 20:34:22 +0100

    chore: migrate to toml4j for licensing reasons

--- src/main/java/dev/dvoa/yarl/Configs.java
+++ src/main/java/dev/dvoa/yarl/Configs.java
@@ -1,48 +1,46 @@
 package dev.dvoa.yarl;
 
-import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
 import net.fabricmc.loader.api.FabricLoader;
 
-import java.nio.file.Path;
+import java.io.File;
+import java.io.IOException;
 
 public class Configs {
 
-    private static CommentedFileConfig CONFIG;
-
     public static String IMPORTANT;
     public static boolean LOAD_RESOURCES;
+    private static File CONFIG_FILE;
 
     public static void load() {
-        Path path = FabricLoader.getInstance()
+        CONFIG_FILE = FabricLoader.getInstance()
                 .getConfigDir()
-                .resolve("yarl.toml");
+                .resolve("yarl.toml")
+                .toFile();
 
-        CONFIG = CommentedFileConfig.builder(path)
-                .autosave()
-                .sync()
-                .build();
+        Toml toml;
+        if (CONFIG_FILE.exists()) {
+            toml = new Toml().read(CONFIG_FILE);
+        } else {
+            toml = new Toml();
+        }
 
-        CONFIG.load();
-
-        // Default values
-        IMPORTANT = CONFIG.getOrElse("IMPORTANT",
-                "DO NOT SHIP THIS CONFIG FILE IN A MODPACK!");
-
-        LOAD_RESOURCES = CONFIG.getOrElse("loadResources", true);
-
-        // Write defaults if missing
-        CONFIG.set("IMPORTANT", IMPORTANT);
-        CONFIG.set("loadResources", LOAD_RESOURCES);
-
-        CONFIG.setComment("loadResources",
-                "- USED INTERNALLY! Stores whether is the first time opening the game.");
-
-        CONFIG.save();
+        IMPORTANT = toml.getString("IMPORTANT", "DO NOT SHIP THIS CONFIG FILE IN A MODPACK!");
+        LOAD_RESOURCES = toml.getBoolean("loadResources", true);
+        save();
     }
 
     public static void save() {
-        CONFIG.set("IMPORTANT", IMPORTANT);
-        CONFIG.set("loadResources", LOAD_RESOURCES);
-        CONFIG.save();
+        TomlWriter writer = new TomlWriter();
+        java.util.Map<String, Object> data = new java.util.HashMap<>();
+        data.put("IMPORTANT", IMPORTANT);
+        data.put("loadResources", LOAD_RESOURCES);
+
+        try {
+            writer.write(data, CONFIG_FILE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
